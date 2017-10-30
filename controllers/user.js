eef6d5017473bd247a3ec8557d1c60de2f42ffcc
dr-binaryV1 <developerdamian@gmail.com>
2017-10-29 23:21:15 -0500

    Add Itinery Controls

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ const User = UserModels.user;
 const Cart = OrderModels.cart;
 const Traveler = UserModels.traveler;
 const Shipping = UserModels.shipping;
+const Itinerary = UserModels.travelItinerary;
 
 exports.getUser = (req, res, next) => {
   User.findOne({"_id": req.get('userId')}, null, (err, user) => {
@@ -19,7 +20,7 @@ exports.getUser = (req, res, next) => {
       mailingCountry: user.mailingCountry,
       mailingZip: user.mailingZip,
       userTypeId: user.userTypeId,
-      intineraryIds: user.intineraryIds,
+      itineraryIds: user.itineraryIds,
       primaryShippingAddress: user.primaryShippingAddress,
       shippingAddressIds: user.shippingAddressIds,
       traveler: user.traveler,
@@ -42,6 +43,8 @@ exports.addShipping = (req, res, next) => {
     if(err) { return next(err); }
 
     User.findOne({"_id": req.get('user')}, null, (err, user) => {
+      if(err) { return next(err); }
+
       user.shippingAddressIds.push(shipping._id);
       if(user.shippingAddressIds.length === 1) {
         user.primaryShippingAddress = shipping._id
@@ -60,7 +63,7 @@ exports.addShipping = (req, res, next) => {
           mailingZip: user.mailingZip,
           userTypeId: user.userTypeId,
           traveler: user.traveler,
-          intineraryIds: user.intineraryIds,
+          itineraryIds: user.itineraryIds,
           primaryShippingAddress: user.primaryShippingAddress,
           shippingAddressIds: user.shippingAddressIds,
           email: user.email
@@ -70,6 +73,53 @@ exports.addShipping = (req, res, next) => {
           if(err) { return next(err); }
 
           res.json({user: data, addresses});
+        });
+      });
+    });
+  });
+};
+
+exports.addItinerary = (req, res, next) => {
+  const itinerary = new Itinerary({
+    departureCity: req.body.departureCity,
+    departureDate: req.body.departureDate,
+    departureTime: req.body.departureTime,
+    arrivalCity: req.body.arrivalCity,
+    arrivalDate: req.body.arrivalDate,
+    arrivalTime: req.body.arrivalTime
+  });
+
+  itinerary.save((err, itinerary) => {
+    if(err) { return next(err); }
+
+    User.findOne({"_id": req.get('user')}, null, (err, user) => {
+      if(err) { return next(err); }
+
+      user.itineraryIds.push(itinerary._id);
+      user.save((err, user) => {
+        if(err) { return next(err); }
+
+        const data = {
+          firstname: user.firstname,
+          lastname: user.lastname,
+          username: user.username,
+          mailingAddress1: user.mailingAddress1,
+          mailingAddress2: user.mailingAddress2,
+          mailingCity: user.mailingCity,
+          mailingCountry: user.mailingCountry,
+          mailingZip: user.mailingZip,
+          userTypeId: user.userTypeId,
+          traveler: user.traveler,
+          itineraryIds: user.itineraryIds,
+          primaryShippingAddress: user.primaryShippingAddress,
+          shippingAddressIds: user.shippingAddressIds,
+          email: user.email
+        };
+
+        Itinerary.find({"_id": data.itineraryIds.map(id => {return id})}, null, (err, itineraries) => {
+          if(err) { return next(err); }
+
+          res.json({user: data, itineraries});
         });
       });
     });
@@ -95,15 +145,49 @@ exports.getShippingAddress = (req, res, next) => {
   });
 };
 
+exports.getTravelItinerary = (req, res, next) => {
+  Itinerary.find({"_id": req.body.itineraryIds.map(id => {return id})}, null, (err, itineraries) => {
+    if(err) { return next(err); }
+    res.json({travelItinerary: itineraries });
+  });
+};
+
+exports.removeTravelItinerary = (req, res, next) => {
+  Itinerary.remove({"_id": req.body.itineraryId}, (err) => {
+    if(err) { return next(err); }
+
+    User.findOne({"_id": req.get('user')}, null, (err, user) => {
+      if(err) { return next(err); }
+
+      user.itineraryIds = user.itineraryIds.filter(id => {
+        return id !== req.body.itineraryId;
+      });
+
+      Itinerary.find({"_id": user.itineraryIds.map(id => {return id})}, (err, itineries) => {
+        if(err) { return next(err); }
+
+        user.save((err, user) => {
+          if(err) { return next(err); }
+          res.json({user, itineries});
+        });
+      });
+    });
+  });
+};
+
 exports.removeShippingAddress = (req, res, next) => {
   Shipping.remove({"_id": req.body.addressId}, (err) => {
     if(err) { return next(err); }
 
     User.findOne({"_id": req.get('user')}, null, (err, user) => {
       if(err) { return next(err); }
+
       user.shippingAddressIds = user.shippingAddressIds.filter(id => {
         return id !== req.body.addressId;
       });
+      if(user.primaryShippingAddress === req.body.addressId) {
+        user.primaryShippingAddress = '';
+      }
 
       Shipping.find({"_id": user.shippingAddressIds.map(id => {return id})}, null, (err, addresses) => {
         if(err) { return next(err); }
