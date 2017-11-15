144f20fc8b4d57c0dcbdaf36713e106ef129a5f9
damian.wynter <damian.wynter@particularpresence.com>
2017-11-15 12:22:33 -0500

    Remove creating user variable andn pass 'this' to method

--- models/user.js
+++ models/user.js
@@ -68,8 +68,7 @@ userSchema.pre('save', function(next) {
 });
 
 userSchema.methods.comparePassword = function(candidatePassword, callback) {
-    const user = this;
-    bcrypt.compare(candidatePassword, user.password, function(err, isMatch) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
         if(err) { return callback(err); }
 
         callback(null, isMatch);
