8309210c0d88f2fbc2ade67cca043c36bf3b10c0
dr-binaryV1 <developerdamian@gmail.com>
2017-11-18 17:52:38 -0500

    Create invoice parameter Add invoiceId to notificaion schema

--- models/order.js
+++ models/order.js
@@ -21,14 +21,25 @@ const orderSchema = new Schema({
   travelerId: {type: String}
 });
 
+const invoiceSchema = new Schema({
+  buyerId: { type: String, required: true },
+  items: [itemSchema],
+  fee: { type: Number, required: true },
+  total: { type: Number, required: true },
+  status: { type: String, default: 'unpaid' },
+});
+
 const cartSchema = new Schema({
   userId: { type: String, required: true, unique: true },
+  invoice: [invoiceSchema],
   itemIds: [{type: String}]
 });
 
-const orderNotificationSchema = new Schema({
+const notificationSchema = new Schema({
+  type: { type: String, default: 'package' },
   subject: { type: String, required: true },
-  orderId: { type: String, required: true },
+  orderId: { type: String },
+  invoiceId: { type: String },
   items: [itemSchema],
   details: { type: String, required: true },
   status: { type: String, default: 'active' }
@@ -37,11 +48,13 @@ const orderNotificationSchema = new Schema({
 const item_model = mongoose.model('item', itemSchema);
 const cart_model = mongoose.model('cart', cartSchema);
 const order_model = mongoose.model('order', orderSchema);
+const invoice_model = mongoose.model('invoice', invoiceSchema);
 const orderItems_model = mongoose.model('orderItems', orderItemsSchema);
-const order_notif_model = mongoose.model('orderNotification', orderNotificationSchema);
+const notif_model = mongoose.model('notification', notificationSchema);
 
 module.exports.item = item_model;
 module.exports.cart = cart_model;
 module.exports.order = order_model;
+module.exports.invoice = invoice_model;
 module.exports.orderItems = orderItems_model;
-module.exports.orderNotification = order_notif_model;
+module.exports.notification = notif_model;
