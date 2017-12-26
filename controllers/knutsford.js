2d4eb44f9ff02103a08d777afe538d6916488430
Damian Wynter <developerdamian@gmail.com>
2017-12-25 19:05:29 -0500

    Add getKnutsford items to knutsford controller

--- controllers/knutsford.js
+++ controllers/knutsford.js
@@ -6,15 +6,27 @@ const Itinerary = UserModels.travelItinerary;
 const Package = OrderModels.package;
 const Items = OrderModels.item;
 
+exports.getKnutsfordItems = (req, res, next) => {
+  KnutsfordItem.find({}, null, (err, k_items) => {
+    if (err) { return next(err); }
+
+    Order.find({ "_id": k_items.map(item => { return item.orderId }) }, null, (err, orders) => {
+      if (err) { return next(err); }
+
+      res.json({ orders });
+    });
+  });
+};
+
 exports.packageData = (req, res, next) => {
-  Package.findOne({"_id": req.body.packageId}, null, (err, pkg) => {
-    if(err) { return next(err) }
+  Package.findOne({ "_id": req.body.packageId }, null, (err, pkg) => {
+    if (err) { return next(err) }
 
-    User.findOne({"_id": pkg.travelerId}, null, (err, usr) => {
-      if(err) { return next(err) }
+    User.findOne({ "_id": pkg.travelerId }, null, (err, usr) => {
+      if (err) { return next(err) }
 
-      Itinerary.findOne({"_id": usr.itineraryIds.map(id => { return id })}, null, (err, itinerary) => {
-        if(err) { return next(err) }
+      Itinerary.findOne({ "_id": usr.itineraryIds.map(id => { return id }) }, null, (err, itinerary) => {
+        if (err) { return next(err) }
 
         res.json({ pkg, usr, itinerary });
       });
